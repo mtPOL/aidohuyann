8d574ce1b1b6cc13183e50134e161755481ca8c8
mtPOL <156664302+mtPOL@users.noreply.github.com>
2024-01-15 17:47:03 +0700

    Add files via upload

---
+++ huyyann.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "idolhuyann hub" -- Put You Name Script Here
+_G.Logo = "https://create.roblox.com/marketplace/asset/10890456199/donut-png%3Fkeyword=&pageNumber=&pagePosition=" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
